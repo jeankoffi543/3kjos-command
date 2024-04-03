68d2559a79ffc3a7e4cb1be701dc121f892dbd92
jeankoffi543 <jeankoffi543@gmail.com>
2024-04-03 01:36:55 +0000

    KJOSCOM-1: configuration et installation

---
+++ src/CommandServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace kjos\Command;
+
+use Illuminate\Support\ServiceProvider;
+
+class CommandServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        // Load routes, views, migrations, etc.
+    }
+
+    public function register()
+    {
+        // Register bindings, listeners, etc.
+    }
+}
